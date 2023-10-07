dcb82767ac803057b3802c3a2aaf6aef504f584d
Wallaby <avocado34.131@gmail.com>
2023-10-08 06:05:03 +0900

    [Feat] RealmManager 추상화 및 SearchHistoryManager 추가

---
+++ Moview/Moview/Sources/Managers/SearchHistoryManager.swift
@@ -0,0 +1,14 @@
+//
+//  SearchHistoryManager.swift
+//  Moview
+//
+//  Created by 이승기 on 2023/10/08.
+//
+
+import RealmSwift
+import RxSwift
+
+
+final class SearchHistoryManager: RealmManager {
+  typealias Item = SearchHistory
+}
